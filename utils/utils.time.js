c70be2ab35cc86fd2fe40b844aeff79f5155382d
MeetYourCreator <rzshamir@icloud.com>
2022-08-20 08:25:33 -0400

    clean up utils files

--- utils/utils.time.js
+++ utils/utils.time.js
@@ -5,21 +5,21 @@ export const setHour = () => {
   const secondRatio = date.getSeconds() / 60;
   const minuteRatio = (secondRatio + date.getMinutes()) / 60;
   const hourRatio = (minuteRatio + date.getHours()) / 12;
-  return hourRatio
-}
+  return hourRatio;
+};
 
 export const setMinute = () => {
   const date = new Date();
   const secondRatio = date.getSeconds() / 60;
   const minuteRatio = (secondRatio + date.getMinutes()) / 60;
-  return minuteRatio
-}
+  return minuteRatio;
+};
 
 export const setSecond = () => {
   const date = new Date();
   const secondRatio = date.getSeconds() / 60;
-  return secondRatio
-}
+  return secondRatio;
+};
 
 export const showLocalTime = () => {
   let time = new Date();
@@ -31,8 +31,8 @@ export const showLocalTime = () => {
   minute = minute < 10 ? '0' + minute : minute;
   second = second < 10 ? '0' + second : second;
 
-  return hour + ':' + minute + ':' + second
-}
+  return hour + ':' + minute + ':' + second;
+};
 
 export const getAllTimeZones = async () => {
   const url = `https://api.timezonedb.com/v2.1/list-time-zone?key=7KJHTP7QS6J7&format=json`;
@@ -41,17 +41,17 @@ export const getAllTimeZones = async () => {
   // console.log('%c%s', 'color: green; background: yellow', 'response:', response)
 
   return response.data.zones;
-}
+};
 
 export const showISO = (timestampMS, styleLight, styleDark) => {
   const zoneISO = new Date(timestampMS * 1000).toISOString();
   const zoneISOhhmmss = zoneISO.slice(11, 16)
   return zoneISOhhmmss
-}
+};
 
 export const angleClock = (hour, minutes) => {
-  const minuteAngle = minutes * 6
-  const hourAngle = (hour * 30) + minutes * 0.5
-  const angle = Math.abs(hourAngle - minuteAngle)
-  return Math.min(angle, 360 - angle)
-}
+  const minuteAngle = minutes * 6;
+  const hourAngle = (hour * 30) + minutes * 0.5;
+  const angle = Math.abs(hourAngle - minuteAngle);
+  return Math.min(angle, 360 - angle);
+};
