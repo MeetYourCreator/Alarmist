a4a4f07bc7190dafacf8fe0fa05e889a88955a67
MeetYourCreator <rzshamir@icloud.com>
2022-09-02 14:03:05 -0400

    init DigitalClock component
    mvp DigitalCLock.js
    
    import DigitalClock component into HomeScreen.js

---
+++ src/components/DigitalClock.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { showLocalTime } from '../utils/utils.time.js';
+// import { DigitalClockContainer } from '../../components/styled/digital/DigitalClockContainer.js';
+// import { Clock } from '../../components/styled/digital/Clock.js';
+// import { ClockText } from '../../components/styled/digital/ClockText.js';
+// import { Location } from '../../components/styled/digital/Location.js';
+// import AlarmDetail from '../../components/alarm/AlarmDetail.jsx';
+
+// import DigitalClockNumberColorModal from '../../components/modals/DigitalClockNumberColorModal.jsx';
+// import DigitalClockNumberFontModal from '../../components/modals/DigitalClockNumberFontModal.jsx';
+// import AlarmModal from '../../components/modals/AlarmModal.jsx';
+
+// import { useSelector } from 'react-redux';
+
+const LocalDigitalClock = () => {
+  const [localTime, setLocalTime] = useState(showLocalTime());
+  const [colorValue, setColorValue] = useState('#0000FF');
+  // const [fontValue, setFontValue] = useState('press-start-2p');
+
+  // const alarms = useSelector((state) => state.alarms.activeAlarms);
+
+  const colorHandler = (event) => {
+    setColorValue(event);
+  };
+  // const fontHandler = (event) => setFontValue(event);
+  const alarmHandler = (event) => setAlarmValue(event);
+
+  // const currentFont = fontValue;
+  const currentColor = colorValue;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLocalTime(showLocalTime());
+    }, 1000);
+    return () => clearTimeout(timeout);
+  });
+
+  return (
+    <>
+      <SafeAreaView style={ styles.screenContainer }>
+        <View style={ styles.clockContainer }>
+          <View style={ styles.clock }>
+
+            <Text
+              style={ [
+                styles.clockText,
+                {
+                  color: currentColor,
+                  textShadowColor: currentColor,
+                  // fontFamily: currentFont,
+                },
+              ] }>
+              { localTime }
+            </Text>
+          </View>
+          <Text style={ styles.location }>LocalTime</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  clockContainer: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 40,
+    marginTop: 180,
+    marginBottom: 90,
+  },
+  clock: {
+    position: 'relative',
+    width: 350,
+    height: 100,
+    borderWidth: 8,
+    borderColor: '#2d2d2d',
+    borderRadius: 15,
+    backgroundColor: '#151617',
+  },
+  clockText: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    textAlign: 'center',
+    fontSize: 35,
+    textShadowColor: '#f70300',
+  },
+  location: {
+    marginTop: 15,
+    marginRight: 7,
+    marginBottom: 5,
+    marginLeft: 7,
+    borderTopWidth: 2,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+  },
+  alarmsContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '70%',
+    height: 170,
+    marginTop: 'auto',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+  menu: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-end',
+    marginTop: 0,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 0,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+export default LocalDigitalClock;
---
+++ src/components/buttons/AppButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+
+const AppButton = ({ color, fontColor, onPress, title }) => {
+  return (
+    <>
+      <View style={ styles.ScreenContainer }>
+        <TouchableOpacity
+          onPress={ onPress }
+          activeOpacity={ 0.5 }
+          style={ [ styles.appButtonContainer, { backgroundColor: `${ color }` } ] }
+        >
+          <Text style={ [ styles.buttonText, { color: `${ fontColor }` } ] }>{ title }</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  screenContainer: {},
+  appButtonContainer: {
+    height: '100%',
+    width: '100%',
+    borderRadius: 0,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    margin: 0,
+  },
+  buttonText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    padding: 20,
+    marginTop: 'auto',
+    writingDirection: 'ltr',
+  },
+});
+export default AppButton;
